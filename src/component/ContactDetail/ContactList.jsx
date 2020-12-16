adaa378e348834c41e8ec2df1af2e3265f86c53a
Shivendra Das <shivendra_das@rediffmail.com>
2020-12-16 20:32:22 +0530

    Add contact list button on chat tab

---
+++ src/component/ContactDetail/ContactList.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Contacts from 'react-native-contacts';
+//import { Icon } from 'react-native-elements'
+import { PERMISSIONS, check, request, RESULTS } from 'react-native-permissions'
+
+class ContactList extends Component {
+    constructor(props) {
+        super(props);
+    }
+    async getContactListPermission() {
+        try {
+            var res = await check(PERMISSIONS.ANDROID.READ_CONTACTS);
+            if (res == RESULTS.DENIED) {
+                res = await request(PERMISSIONS.ANDROID.READ_CONTACTS);
+            } 
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.touchableOpacityStyle}  onPress={async () => { await this.getContactListPermission() }}> 
+                    <Text style = {styles.floatingButtonStyle}>+</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+}
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5F5F5'
+    },
+
+    touchableOpacityStyle: {
+        borderWidth: 1,
+        borderColor: 'rgba(0,0,0,0.2)',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        position: 'absolute',
+        bottom: 10,
+        right: 10,
+        height: 50,
+        backgroundColor: '#fff',
+        borderRadius: 100
+    },
+
+    floatingButtonStyle: {
+        fontSize:30
+    }
+});
+export default ContactList;
--- src/component/tab/ChatTab.jsx
+++ src/component/tab/ChatTab.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NativeModules, PermissionsAndroid } from 'react-native';
 import {
   TouchableOpacity,
   StyleSheet,
@@ -6,6 +7,7 @@ import {
   Text,
   SafeAreaView
 } from 'react-native';
+import ContactList from '../ContactDetail/ContactList';
 
 const styles = StyleSheet.create({
   button: {
@@ -21,46 +23,7 @@ class ChatTab extends React.Component {
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ flex: 1, padding: 16 }}>
-          <View
-            style={{
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text
-              style={{
-                fontSize: 25,
-                textAlign: 'center',
-                marginBottom: 16
-              }}>
-              Setting{'\n'}(You are on Chat Page)
-          </Text>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={
-                () => this.props.navigation.navigate('HomeTab')
-              }>
-              <Text>Go to Home Tab</Text>
-            </TouchableOpacity>
-          </View>
-          <Text
-            style={{
-              fontSize: 18,
-              textAlign: 'center',
-              color: 'grey'
-            }}>
-            React Native Tab Navigation
-        </Text>
-          <Text
-            style={{
-              fontSize: 16,
-              textAlign: 'center',
-              color: 'grey'
-            }}>
-            www.aboutreact.com
-        </Text>
-        </View>
+        <ContactList></ContactList>
       </SafeAreaView>
     );
   }
