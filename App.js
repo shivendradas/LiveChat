248106a227d388dfa8e361fdba086a0f6390f292
Shivendra Das <shivendra_das@rediffmail.com>
2020-12-17 19:45:05 +0530

    add react-native-paper provider

--- App.js
+++ App.js
@@ -8,16 +8,19 @@
 import 'react-native-gesture-handler';
 
 import * as React from 'react';
-import { Provider } from 'react-redux'
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
 import store from './src/store/store'
 import Authentication from './src/component/login/Authentication';
 
 class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <Authentication />
-      </Provider>      
+      <StoreProvider store={store}>
+        <PaperProvider>
+          <Authentication />
+        </PaperProvider>
+      </StoreProvider>
     );
   }
 }
