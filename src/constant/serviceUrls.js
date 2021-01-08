3841503b7dbd6e3865615051101559734eace5e8
Shivendra Das <shivendra_das@rediffmail.com>
2021-01-08 22:36:42 +0530

    link to heroku server for auth service

--- src/constant/serviceUrls.js
+++ src/constant/serviceUrls.js
@@ -1,5 +1,7 @@
-export const LOGIN_REGISER_URL = "http://192.168.43.46:3001/api/userDetail";
-export const LOGIN_URL = "http://192.168.43.46:3001/api/userDetail/login";
+/*export const LOGIN_REGISER_URL = "http://192.168.43.46:3001/api/userDetail";
+export const LOGIN_URL = "http://192.168.43.46:3001/api/userDetail/login";*/
+export const LOGIN_REGISER_URL = "https://livechatauthservice.herokuapp.com/api/userDetail";
+export const LOGIN_URL = "https://livechatauthservice.herokuapp.com/api/userDetail/login";
 
 
 
